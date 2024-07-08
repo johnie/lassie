830d3f7376d35f01eacd1b63b2803c73dec4852d
Johnie Hjelm <johnie@hjelm.im>
2024-07-08 11:48:26 +0200

    feat(constants): Add constants

---
+++ src/constants.ts
@@ -0,0 +1,18 @@
+export const REGULAR_THRESHOLD_DAYS = 2;
+export const CHILDRENS_THRESHOLD_DAYS = 3;
+export const NEW_RELEASE_BONUS_THRESHOLD_DAYS = 2;
+
+export const REGULAR_BASE_PRICE = 2;
+export const CHILDRENS_BASE_PRICE = 1.5;
+
+export const NEW_RELEASE_DAILY_RATE = 3;
+export const EXTENDED_RENTAL_DAILY_RATE = 1.5;
+
+export const BASE_FREQUENT_RENTER_POINTS = 1;
+export const BONUS_FREQUENT_RENTER_POINTS = 1;
+
+export const MOVIE_CODES = {
+  REGULAR: 'regular',
+  NEW: 'new',
+  CHILDRENS: 'childrens',
+} as const;
