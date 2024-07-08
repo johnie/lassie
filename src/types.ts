24f39446766fac6e8bb62dae4e9901556eb996e0
Johnie Hjelm <johnie@hjelm.im>
2024-07-08 12:20:14 +0200

    feat(types): Add types

---
+++ src/types.ts
@@ -0,0 +1,33 @@
+import { MOVIE_CODES } from '@/constants.js';
+
+export type MovieCode = (typeof MOVIE_CODES)[keyof typeof MOVIE_CODES];
+
+export type Movie = {
+  title: string;
+  code: MovieCode;
+};
+
+export type Rental = {
+  movieID: string;
+  days: number;
+};
+
+export type RentalStatement = {
+  statement: string;
+  errors: string[];
+};
+
+export type RentalCalculation = {
+  movie: Movie;
+  amount: number;
+  points: number;
+};
+
+export type Customer = {
+  name: string;
+  rentals: Rental[];
+};
+
+export type MoviesDatabase = {
+  [key: string]: Movie;
+};
